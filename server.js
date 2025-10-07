028c79af42fafddfe9921987f211dc8daf6c4dc9
tashaxanavsem82 <swgwull@hotmail.com>
2025-10-07 13:48:41 -0500

    Ensure MongoDB URI is provided in .env file

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ const io = socketIo(server);
 app.use(cors());
 app.use(express.json());
 
+if (!process.env.MONGODB_URI) {
+  console.error('MONGODB_URI is not defined in .env file');
+  process.exit(1);
+}
+
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
