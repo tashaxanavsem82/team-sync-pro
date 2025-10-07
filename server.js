59048d4f1bda6f2bf15b104a6ae22edffea9082d
tashaxanavsem82 <swgwull@hotmail.com>
2025-10-07 13:48:10 -0500

    Improve MongoDB connection error logging

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+  .catch(err => console.error('MongoDB connection error:', err));
 
 app.get('/', (req, res) => {
   res.send('Server is running!');
